565bde1dfc2e8474fb231603820de7249429b412
K-Kazum1 <mitchwiig@gmail.com>
2017-11-04 00:36:31 -0400

    put scripts in folders, implemented pathfinding, and starting walls

---
+++ Assets/Scripts/Pathfinding/TileEdge.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileEdge{
+
+	public TileNode node;
+    public float cost;
+
+}
---
+++ Assets/Scripts/objects/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+    TerrainTile startTile;
+    TerrainTile endTile;
+    List<TerrainTile> path;
+    TerrainManager tm = GameObject.FindGameObjectWithTag("TerrainManager").GetComponent<TerrainManager>();
+    public Wall(TerrainTile s,TerrainTile e)
+    {
+        startTile = s;
+        endTile = e;
+        if (s.x > e.x)
+        {
+            for (int i = s.x; i < e.x; i--)
+            {
+
+            }
+        }
+        for(int i = s.x; i <= e.x; i++)
+        {
+            
+        }
+    }
+}
