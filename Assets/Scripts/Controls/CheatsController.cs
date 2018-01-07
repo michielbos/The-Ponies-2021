520210d0212adc18c36821afb8fae2d1463dc29c
Michiel Bos <michielbos97@hotmail.com>
2018-01-07 15:59:15 +0100

    Added console panel that can be shown with the 'expand' or 'help' command.

--- Assets/Scripts/Controls/CheatsController.cs
+++ Assets/Scripts/Controls/CheatsController.cs
@@ -5,35 +5,70 @@ using UnityEngine.UI;
 
 public class CheatsController : MonoBehaviour {
 	public InputField cheatField;
+	public RectTransform consolePanel;
 	public HUDController hudController;
+	private bool visible = false;
+	private bool expanded = false;
 	private bool hadFocus = false;
+	private string consoleContent = "";
+	private float initialCheatFieldX, initialCheatFieldY, initialCheatFieldWidth;
 
 	void Start () {
-		
+		RectTransform rectTransform = cheatField.GetComponent<RectTransform>();
+		initialCheatFieldX = rectTransform.position.x;
+		initialCheatFieldY = rectTransform.position.y;
+		initialCheatFieldWidth = rectTransform.rect.width;
 	}
 
 	void Update () {
 		//It seems Unity doesn't accept the C key while ctrl is pressed...
 		if (/*Input.GetKey(KeyCode.LeftControl) &&*/ Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.C)) {
-			setCheatFieldVisible(!cheatField.enabled);
+			setCheatFieldVisible(!visible);
 		}
-		if (cheatField.enabled && hadFocus && Input.GetKey(KeyCode.Return)) {
-			Debug.Log(enterCheat(cheatField.text));
-			setCheatFieldVisible(false);
+		if (visible && hadFocus && Input.GetKey(KeyCode.Return)) {
+			bool wasExpanded = expanded;
+			if (cheatField.text.Length > 0) {
+				enterCheat(cheatField.text);
+			}
+			cheatField.text = "";
+			if (expanded || wasExpanded) {
+				cheatField.ActivateInputField();
+			} else {
+				setCheatFieldVisible(false);
+			}
 		}
 		hadFocus = cheatField.isFocused;
 	}
 		
 	void setCheatFieldVisible (bool visible) {
-		//The active/enabled flag won't hide it, so we're just shrinking it to zero for now...
+		this.visible = visible;
 		cheatField.interactable = visible;
-		cheatField.enabled = visible;
+		//The active/enabled flag won't hide it, so we're just shrinking it to zero for now...
 		if (visible) {
-			cheatField.transform.localScale = new Vector3 (1, 1, 1);
+			cheatField.transform.localScale = new Vector3(1, 1, 1);
 			cheatField.ActivateInputField();
+			if (expanded) {
+				consolePanel.localScale = new Vector3(1, 1, 1);
+			}
 		} else {
 			cheatField.text = "";
-			cheatField.transform.localScale = new Vector3 (0, 0, 0);
+			cheatField.transform.localScale = new Vector3(0, 0, 0);
+			consolePanel.localScale = new Vector3(0, 0, 0);
+		}
+	}
+
+	void setExpanded (bool expanded) {
+		this.expanded = expanded;
+		if (visible) {
+			consolePanel.localScale = new Vector3(1, 1, 1);
+		}
+		RectTransform cheatFieldTransform = cheatField.GetComponent<RectTransform>();
+		if (expanded) {
+			cheatFieldTransform.anchoredPosition = new Vector2(0, -consolePanel.rect.height);
+			cheatFieldTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, consolePanel.rect.width);
+		} else {
+			cheatFieldTransform.position = new Vector2(initialCheatFieldX, initialCheatFieldY);
+			cheatFieldTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, initialCheatFieldWidth);
 		}
 	}
 
@@ -41,7 +76,7 @@ public class CheatsController : MonoBehaviour {
 		string[] split = cheat.Split(new char[]{' '}, 2);
 		string[] parameters;
 		if (split.Length > 1) {
-			parameters = split[1].Split(new char[]{ ' ' });
+			parameters = split[1].Split(new char[]{' '});
 		} else {
 			parameters = new string[0];
 		}
@@ -49,18 +84,25 @@ public class CheatsController : MonoBehaviour {
 	}
 
 	bool tryCheat (string command, string[] parameters) {
+		Debug.Log("did " + command);
+		//Money cheats
 		if (command == "rosebud")
 			hudController.ChangeFunds(1000);
 		else if (command == "motherload")
 			hudController.ChangeFunds(50000);
 		else if (command == "filthyrich")
 			hudController.ChangeFunds(1000000);
+		else if (command == "adjustfunds" && parameters.Length == 1)
+			return changeFundsCheat(parameters [0]);
+		else if (command == "addfunds" && parameters.Length == 1)
+			return addFundsCheat(parameters [0]);
+		//Misc cheats
+		else if (command == "expand")
+			setExpanded(!expanded);
+		else if (command == "help")
+			showHelp();
 		else if (command == "forcequit")
 			Application.Quit();
-		else if (command == "adjustfunds" && parameters.Length == 1)
-			return changeFundsCheat(parameters[0]);
-		else if (command == "addfunds" && parameters.Length == 1)
-			return addFundsCheat(parameters[0]);
 		else
 			return false;
 		return true;
@@ -82,5 +124,10 @@ public class CheatsController : MonoBehaviour {
 			return true;
 		} else
 			return false;
+	}
+
+	void showHelp () {
+		setExpanded(true);
+		//TODO: Display help.
 	}
 }
