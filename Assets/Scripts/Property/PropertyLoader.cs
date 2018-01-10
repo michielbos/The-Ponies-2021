1c45bc01b24e446fe3fd89b6944e875e5b98d3bf
Michiel Bos <M.Bos6@student.han.nl>
2018-01-10 18:48:04 +0100

    PropertDatas can now be read and written by ID.

--- Assets/Scripts/Property/PropertyLoader.cs
+++ Assets/Scripts/Property/PropertyLoader.cs
@@ -8,36 +8,53 @@ public class PropertyLoader : MonoBehaviour {
 	public PropertyData loaded;
 	public bool writeToFile;
 	public bool readFromFile;
+	const string dataPath = "Assets/Resources/SaveData/";
+	const string propertyPath = dataPath + "Property/";
+
+	void Start () {
+		TryCreateDirectory(propertyPath);
+	}
 
 	void Update () {
 		if (writeToFile) {
 			writeToFile = false;
-			WriteFile();
+			SaveProperty(toSave);
 		}
 		if (readFromFile) {
 			readFromFile = false;
-			ReadFile();
+			loaded = LoadProperty(toSave.id);
 		}
 	}
+
+	void SaveProperty (PropertyData propertyData) {
+		WriteFile(propertyPath + "Property" + toSave.id, JsonUtility.ToJson(toSave));
+	}
+
+	PropertyData LoadProperty (int id) {
+		return JsonUtility.FromJson<PropertyData>(ReadFile(propertyPath + "Property" + id));
+	}
 		
-	void WriteFile() {
-		string path = "Assets/Resources/test.txt";
+	void WriteFile (string path, string data) {
 		if (File.Exists(path))
 			File.Delete(path);
 		StreamWriter writer = new StreamWriter(path, true);
-		//writer.WriteLine(JsonUtility.ToJson(toSave));
-		string data = JsonUtility.ToJson(toSave);
 		Debug.Log("Writing data: " + data);
 		writer.WriteLine(data);
 		writer.Close();
 	}
 		
-	void ReadFile() {
-		string path = "Assets/Resources/test.txt";
+	string ReadFile (string path) {
 		StreamReader reader = new StreamReader(path);
 		string data = reader.ReadToEnd();
 		Debug.Log("Reading data: " + data);
 		reader.Close();
-		loaded = JsonUtility.FromJson<PropertyData>(data);
+		return data;
+	}
+
+	void TryCreateDirectory (string path) {
+		if (!Directory.Exists(path)) {
+			Debug.Log("Created directory: " + path);
+			Directory.CreateDirectory(path);
+		}
 	}
 }
