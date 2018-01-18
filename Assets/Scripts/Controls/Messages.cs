56ab08869884a466aac20c00b1804915aea829f2
Michiel Bos <michielbos97@hotmail.com>
2018-01-18 22:49:18 +0100

    Added message window functionality.

---
+++ Assets/Scripts/Controls/Messages.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Messages : MonoBehaviour {
+	public MessageWindow messageWindowPrefab;
+	public AudioSource audioSource;
+	public List<MessageWindow> messageWindows;
+	public bool ad = false;
+	public bool rem = false;
+	public int counter = 0;
+
+	public Messages () {
+		messageWindows = new List<MessageWindow>();
+	}
+
+	public MessageWindow SendMessage (string title, string content) {
+		return SendQuestion(title, content, "OK");
+	}
+
+	public MessageWindow sendConfirm (string title, string content) {
+		return SendQuestion(title, content, "No", "Yes");
+	}
+
+	public MessageWindow SendQuestion (string title, string content, params string[] buttons) {
+		MessageWindow window = Instantiate(messageWindowPrefab, transform);
+		window.Initialize(this, audioSource, title, content, buttons);
+		if (messageWindows.Count > 0) {
+			window.gameObject.SetActive(false);
+		}
+		messageWindows.Add(window);
+		return window;
+	}
+
+	public void OnMessageClosed (int button) {
+		messageWindows.RemoveAt(0);
+		if (messageWindows.Count > 0)
+			messageWindows[0].gameObject.SetActive(true);
+	}
+}
