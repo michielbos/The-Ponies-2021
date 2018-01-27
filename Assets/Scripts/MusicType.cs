fa647534c58dc24bc18f4c41a67e6467a1ebbd50
Michiel Bos <michielbos97@hotmail.com>
2018-01-27 20:40:58 +0100

    Forgot to commit MusicType, oops.

---
+++ Assets/Scripts/MusicType.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// Enum with all the music types.
+/// </summary>
+public enum MusicType {
+    NoMusic,
+    Neighbourhood,
+    CreateAPony,
+    BuyMode,
+    BuildMode,
+    CommunityBuildMode
+}
