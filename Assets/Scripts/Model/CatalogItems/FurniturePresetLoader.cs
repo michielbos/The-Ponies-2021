870397f6f40331d966016a9ea15af453a1ae68f8
Michiel Bos <michielbos97@hotmail.com>
2018-02-10 23:37:06 +0100

    Did some refactoring, FurniturePresetLoader now extends PresetLoader which does most of the work. Presets now extends a Preset class to work with the PresetLoader.

--- Assets/Scripts/Model/CatalogItems/FurniturePresetLoader.cs
+++ Assets/Scripts/Model/CatalogItems/FurniturePresetLoader.cs
@@ -1,86 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// Class for loading the furniture presets from the resources, as well as the mod packages.
 /// </summary>
-public class FurniturePresetLoader {
-	const string contentPath = "Assets/Resources/Content/";
-	const string furniturePath = contentPath + "Furniture/";
+public class FurniturePresetLoader : PresetLoader<FurniturePreset, FurniturePresetData> {
+	const string furnitureFolder = "Furniture/";
 	const string furnitureExtension = ".furniture";
 
-	public FurniturePresetLoader () {
-		TryCreateDirectory(furniturePath);
+	public FurniturePresetLoader () : 
+		base(furnitureFolder, furnitureExtension) { }
+
+	public override FurniturePreset CreateFromData (FurniturePresetData presetData) {
+		return new FurniturePreset(presetData);
 	}
 
-	/// <summary>
-	/// Load all furniture presets, from both the resources and the mod packages.
-	/// </summary>
-	/// <returns>A dictionary with all loaded furniture presets.</returns>
-	public Dictionary<Guid, FurniturePreset> LoadAllPresets () {
-		Dictionary<Guid, FurniturePreset> presets = new Dictionary<Guid, FurniturePreset>();
-		LoadResourcePresets(presets);
-		LoadModPresets(presets);
-		return presets;
-	}
-
-	private void LoadResourcePresets (Dictionary<Guid, FurniturePreset> presets) {
-		string[] files = Directory.GetFiles(furniturePath);
-		foreach (string s in files) {
-			if (!s.EndsWith(furnitureExtension))
-				continue;
-			FurniturePreset preset = LoadPreset(s);
-			uint packIdentifier = GuidUtil.GetPackIdentifier(preset.guid);
-			if (packIdentifier != 0) {
-				Debug.LogWarning("Furniture preset in file " + s + " has pack identifier " + packIdentifier + " instead of 0.");
-			}
-			AddFurniturePreset(presets, s, preset);
-		}
-	}
-
-	private void LoadModPresets (Dictionary<Guid, FurniturePreset> presets) {
-		foreach (Mod mod in ModLoader.GetLoadedMods()) {
-			AssetBundle bundle = mod.assetBundle;
-			foreach (string assetName in bundle.GetAllAssetNames()) {
-				if (!assetName.StartsWith("assets/furniture/"))
-					continue;
-				TextAsset textAsset = bundle.LoadAsset<TextAsset>(assetName);
-				if (textAsset == null) {
-					Debug.Log(assetName + " is not a furniture asset. Skipping.");
-					continue;
-				}
-				try {
-					FurniturePreset preset =
-						new FurniturePreset(JsonUtility.FromJson<FurniturePresetData>(textAsset.text)) {assetBundle = bundle};
-					uint packId = GuidUtil.GetPackIdentifier(preset.guid);
-					if (packId == mod.modInfoData.packId) {
-						AddFurniturePreset(presets, assetName, preset);
-					} else {
-						Debug.LogWarning("Furniture item " + assetName + " in " + bundle.name + " has packId " + packId + " while the mod has packId " + mod.modInfoData.packId + ". Skipping.");
-					}
-				} catch (Exception e) {
-					Debug.LogError("Exception when trying to load furniture item " + assetName + " from " + bundle.name + "! Not loading furniture item.");
-					Debug.LogException(e);
-				}
-			}
-		}
-	}
-	
-	private void AddFurniturePreset (Dictionary<Guid, FurniturePreset> presets, string file, FurniturePreset preset) {
-		if (!presets.ContainsKey(preset.guid)) {
-			if (ValidateFurniturePreset(preset)) {
-				presets.Add(preset.guid, preset);
-			} else {
-				Debug.LogWarning("Furniture item " + preset.guid + " (" + file + ") didn't validate. Not loading.");
-			}
-		} else {
-			Debug.LogWarning("Furniture item with GUID " + preset.guid + " already exists. Not loading file " + file + ".");
-		}
-	}
-
-	private bool ValidateFurniturePreset (FurniturePreset preset) {
+	protected override bool ValidatePreset (FurniturePreset preset) {
 		//We are probably still missing a lot of checks.
 		if (preset.furnitureSkins == null || preset.furnitureSkins.Length <= 0) {
 			Debug.LogWarning("Preset has no furniture skins.");
@@ -89,25 +23,4 @@ public class FurniturePresetLoader {
 		return true;
 	}
 
-	private FurniturePreset LoadPreset (string path) {
-		return new FurniturePreset(LoadFurniturePresetData(path));
-	}
-
-	private FurniturePresetData LoadFurniturePresetData (string path) {
-		return JsonUtility.FromJson<FurniturePresetData>(ReadFile(path));
-	}
-		
-	private string ReadFile (string path) {
-		StreamReader reader = new StreamReader(path);
-		string data = reader.ReadToEnd();
-		reader.Close();
-		return data;
-	}
-
-	private void TryCreateDirectory (string path) {
-		if (!Directory.Exists(path)) {
-			Debug.Log("Created directory: " + path);
-			Directory.CreateDirectory(path);
-		}
-	}
 }
---
+++ Assets/Scripts/Model/CatalogItems/Preset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Abstract class for catalog items that can be loaded with a preset loader.
+/// </summary>
+public abstract class Preset : CatalogItem {
+    public readonly Guid guid;
+    public AssetBundle assetBundle;
+
+    protected Preset (Guid guid, string name, string description, int price, ObjectCategory category,
+        NeedStats needStats, SkillStats skillStats, RequiredAge requiredAge) :
+        base(name, description, price, category, needStats, skillStats, requiredAge) {
+        this.guid = guid;
+    }
+
+    protected Preset (Guid guid, string name, string description, int price, ObjectCategory category) :
+        base(name, description, price, category) {
+        this.guid = guid;
+    }
+
+}
