523f91cb5c5ca78a14b23015dce6529f1f20d8ff
Michiel Bos <michielbos97@hotmail.com>
2018-02-11 00:57:42 +0100

    Added FloorPreset, FloorPresetData, FloorPresets manager. Floors are now loaded and displayed in the catalog. Changed GameCanvas prefab GameSceneTest scene to link buildmode buttons to catalog controller.

--- Assets/Scripts/Model/CatalogItems/CatalogItemProvider.cs
+++ Assets/Scripts/Model/CatalogItems/CatalogItemProvider.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -14,8 +13,14 @@ public static class CatalogItemProvider {
 	/// <param name="objectCategory">The ObjectCategory to get the items for.</param>
 	/// <returns>A List with all matching CatalogItems.</returns>
 	public static List<CatalogItem> GetCatalogItems (ObjectCategory objectCategory) {
+		//I wish we could somehow squeeze the casting and sorting into a nice method.
 		if (ObjectCategoryUtil.IsFurnitureCategory(objectCategory)) {
 			List<CatalogItem> catalogItems = FurniturePresets.Instance.GetFurniturePresets(objectCategory).Cast<CatalogItem>().ToList();
+			catalogItems.Sort((a, b) => a.price.CompareTo(b.price));
+			return catalogItems;
+		}
+		if (objectCategory == ObjectCategory.Floors) {
+			List<CatalogItem> catalogItems = FloorPresets.Instance.GetFloorPresets().Cast<CatalogItem>().ToList();
 			catalogItems.Sort((a, b) => a.price.CompareTo(b.price));
 			return catalogItems;
 		}
---
+++ Assets/Scripts/Model/CatalogItems/FloorPreset.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+/// <summary>
+/// Preset for furniture items. These are the objects that are displayed in the catalog.
+/// </summary>
+[Serializable]
+public class FloorPreset : Preset {
+	private readonly string textureName;
+	private Texture texture;
+	private Material material;
+	
+	public FloorPreset (FloorPresetData fpd) : 
+		base(new Guid(fpd.guid), fpd.name, fpd.description, fpd.price, ObjectCategory.Floors) {
+		textureName = fpd.textureName;
+	}
+
+	public override Texture[] GetPreviewTextures () {
+		return new Texture[] {GetTexture()};
+	}
+	
+	private Texture GetTexture () {
+		Debug.Log(textureName);
+		if (texture == null) {
+			if (assetBundle != null) {
+				texture = assetBundle.LoadAsset<Texture>("assets/" + textureName);
+			} else {
+				texture = Resources.Load<Texture>(textureName);
+			}
+		}
+		//TODO: Use placeholder texture if still null...
+		return texture;
+	}
+	
+	private Material GetMaterial () {
+		if (material == null) {
+			//TODO: Use the right shader or use a source material.
+			material = new Material(Shader.Find("Diffuse"));
+		}
+		return material;
+	}
+
+	/// <summary>
+	/// Place a floor GameObject with the texture of this floor preset.
+	/// </summary>
+	/// <param name="prefab">The floor prefab to instantiate.</param>
+	/// <param name="x">The X coordinate of the floor tile.</param>
+	/// <param name="y">The Y coordinate of the floor tile.</param>
+	public GameObject PlaceFloor (GameObject prefab, int x, int y) {
+		GameObject gameObject = Object.Instantiate(prefab);
+		return gameObject;
+	}
+
+}
