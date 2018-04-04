4e51f7bd366d5808ec2ee041c7eeb0fd79aa6c2f
Simon Elberich <simonelberich@gmail.com>
2018-04-04 22:12:55 +0200

    Sound Fix #2, audio simply had too few channels for two sounds

--- Assets/Scripts/Controllers/Singletons/SoundController.cs
+++ Assets/Scripts/Controllers/Singletons/SoundController.cs
@@ -2,63 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using Assets.Scripts.Util;
+using UnityEditor;
 using UnityEngine;
 
-namespace Assets.Scripts.Controllers
-{
-	public enum SoundType
-	{
-		Undefined,
-		Sell,
-		Buy, 
-		Deny,
-		Place,
-		Rotate, 
-		Woosh,
-		Click
-	}
+namespace Assets.Scripts.Controllers {
+    public enum SoundType {
+        Undefined,
+        Sell,
+        Buy,
+        Deny,
+        Place,
+        Rotate,
+        Woosh,
+        Click
+    }
 
-	[Serializable]
-	public class SoundValuePair
-	{
-		public SoundType type;
-		public AudioClip clip;
-	}
+    [Serializable]
+    public class SoundValuePair {
+        public SoundType type;
+        public AudioClip clip;
+    }
 
-	public class SoundController : SingletonMonoBehaviour<SoundController>
-	{
-		[SerializeField]
-		protected AudioSource audioSource;
-		[SerializeField]
-		protected List<SoundValuePair> clips;
+    public class SoundController : SingletonMonoBehaviour<SoundController> {
+        [SerializeField] protected int chanelCount = 5;
+        [SerializeField] protected List<SoundValuePair> clips;
 
-		public void PlaySound(SoundType type)
-		{
-			if (clips.Count == 0 || type == SoundType.Undefined)
-				return;
-			SoundValuePair[] pairs = clips.Where(c => c.type == type).ToArray();
-			if (pairs.Length == 0)
-				return;
-			SoundValuePair pair = pairs[UnityEngine.Random.Range(0, pairs.Length)];
-			PlaySound(pair.clip);
-		}
+        private AudioSource[] sources;
+        private int currentSource = 0;
 
-		public void PlaySound(string name)
-		{
-			if (clips.Count == 0)
-				return;
-			SoundValuePair[] pairs = clips.Where(c => c.clip != null && c.clip.name == name).ToArray();
-			if (pairs.Length == 0)
-				return;
-			SoundValuePair pair = pairs[UnityEngine.Random.Range(0, pairs.Length)];
-			PlaySound(pair.clip);
-		}
+        public void Start() {
+            if (chanelCount < 2)
+                chanelCount = 2;
 
-		public void PlaySound(AudioClip clip)
-		{
-			if (clip == null)
-				return;
-			audioSource.PlayOneShot(clip);
-		}
-	}
+            sources = new AudioSource[chanelCount];
+            for (int i = 0; i < chanelCount; i++) {
+                sources[i] = Camera.main.gameObject.AddComponent<AudioSource>();
+            }
+        }
+
+        public void PlaySound(SoundType type) {
+            if (clips.Count == 0 || type == SoundType.Undefined)
+                return;
+            SoundValuePair[] pairs = clips.Where(c => c.type == type).ToArray();
+            if (pairs.Length == 0)
+                return;
+            SoundValuePair pair = pairs[UnityEngine.Random.Range(0, pairs.Length)];
+            PlaySound(pair.clip);
+        }
+
+        public void PlaySound(string name) {
+            if (clips.Count == 0)
+                return;
+            SoundValuePair[] pairs = clips.Where(c => c.clip != null && c.clip.name == name).ToArray();
+            if (pairs.Length == 0)
+                return;
+            SoundValuePair pair = pairs[UnityEngine.Random.Range(0, pairs.Length)];
+            PlaySound(pair.clip);
+        }
+
+        public void PlaySound(AudioClip clip) {
+            if (clip == null)
+                return;
+            sources[currentSource].PlayOneShot(clip);
+            currentSource = (currentSource + 1) % chanelCount;
+        }
+    }
 }
