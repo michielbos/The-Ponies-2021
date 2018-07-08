ca5ae01efd6f6df1a672d741439db843034120a8
Michiel Bos <michielbos97@hotmail.com>
2018-07-08 16:36:49 +0200

    The buildmarker can now scale when dragging floors.

--- Assets/Scripts/Controllers/Tools/FloorTool.cs
+++ Assets/Scripts/Controllers/Tools/FloorTool.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scripts.Controllers;
+﻿using System;
+using Assets.Scripts.Controllers;
 using UnityEngine;
 using UnityEngine.Experimental.UIElements;
 
@@ -15,6 +16,7 @@ public class FloorTool : ScriptableObject, ITool
 	private FloorPreset placingPreset;
 	private GameObject buildMarker;
 	private TerrainTile targetTile;
+	private Vector2Int dragPosition;
 	private bool pressingTile;
 	private bool canPlace;
 
@@ -41,16 +43,17 @@ public class FloorTool : ScriptableObject, ITool
 	}
 
 	private void CreateBuildMarker () {
+		if (buildMarker != null) {
+			Destroy(buildMarker);
+		}
 		buildMarker = Instantiate(buildMarkerPrefab);
 		placingPreset.ApplyToGameObject(buildMarker);
 		SetBuildMarkerPosition(0, 0);
 	}
 
 	private void UpdateBuildMarker () {
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		if (!HUDController.Instance.IsMouseOverGui() && Physics.Raycast(ray, out hit, 1000, 1 << LAYER_TERRAIN)) {
-			TerrainTile newTargetTile = hit.collider.GetComponent<TerrainTileDummy>().terrainTile;
+		TerrainTile newTargetTile = GetTileUnderMouse();
+		if (newTargetTile != null) {
 			if (newTargetTile != targetTile) {
 				BuildMarkerMoved(newTargetTile);
 			}
@@ -61,6 +64,15 @@ public class FloorTool : ScriptableObject, ITool
 			buildMarker.transform.position = new Vector3(0, -100, 0);
 			targetTile = null;
 		}
+	}
+
+	private TerrainTile GetTileUnderMouse() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (!HUDController.Instance.IsMouseOverGui() && Physics.Raycast(ray, out hit, 1000, 1 << LAYER_TERRAIN)) {
+			return hit.collider.GetComponent<TerrainTileDummy>().terrainTile;
+		}
+		return null;
 	}
 
 	private void BuildMarkerMoved (TerrainTile newTile) {
@@ -87,9 +99,32 @@ public class FloorTool : ScriptableObject, ITool
 				}
 			} else {
 				PlaceObject();
+				CreateBuildMarker();
 			}
 			pressingTile = false;
+		} else {
+			HandleFloorDragging();
 		}
+	}
+
+	private void HandleFloorDragging() {
+		TerrainTile terrainTile = GetTileUnderMouse();
+		if (terrainTile == null)
+			return;
+		dragPosition = terrainTile.GetPosition();
+		int x1 = Math.Min(targetTile.x, dragPosition.x);
+		int x2 = Math.Max(targetTile.x, dragPosition.x) + 1;
+		int y1 = Math.Min(targetTile.y, dragPosition.y);
+		int y2 = Math.Max(targetTile.y, dragPosition.y) + 1;
+		RectInt dragRect = new RectInt();
+		dragRect.SetMinMax(new Vector2Int(x1, y1), new Vector2Int(x2, y2));
+		Transform markerTransform = buildMarker.transform;
+		markerTransform.localScale = new Vector3(dragRect.width, markerTransform.localScale.y, dragRect.height);
+		markerTransform.position = new Vector3(
+			dragRect.x + dragRect.width / 2f,
+			markerTransform.position.y,
+			dragRect.y + dragRect.height / 2f
+		);
 	}
 
 	private void PlaceObject () {
@@ -119,8 +154,7 @@ public class FloorTool : ScriptableObject, ITool
     public void OnCatalogSelect(CatalogItem item, int skin)
     {
         FloorPreset floorPreset = item as FloorPreset;
-        if (floorPreset == null)
-        {
+        if (floorPreset == null) {
             Debug.LogWarning(item + " is not a FloorPreset, cannot be set to FloorTool.");
             return;
         }
