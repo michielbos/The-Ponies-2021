39ca7db88026ddd0ccc0b1598d466c41a277690a
Michiel Bos <michielbos97@hotmail.com>
2019-01-19 18:46:15 +0100

    PoneCrafterImporter can now read floor data from zip files! :D

--- Assets/Scripts/PoneCrafter/PoneCrafterImporter.cs
+++ Assets/Scripts/PoneCrafter/PoneCrafterImporter.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using PoneCrafter.Json;
 using PoneCrafter.Model;
 using UnityEngine;
@@ -7,9 +8,9 @@ using UnityEngine;
 namespace PoneCrafter {
 
 public class PoneCrafterImporter {
-    public List<Floor> loadedFloors;
     private const string PROPERTIES_FILE = "properties.json";
-    private const string PCC_EXTENSION = ".json";
+    private const string PCC_EXTENSION = ".pcc";
+    public List<Floor> loadedFloors;
 
     public PoneCrafterImporter() {
         loadedFloors = new List<Floor>();
@@ -21,29 +22,57 @@ public class PoneCrafterImporter {
             Directory.CreateDirectory(directory);
         }
         foreach (string file in Directory.GetFiles(directory)) {
-            if (!file.EndsWith("/" + PROPERTIES_FILE)) {
+            if (!file.ToLower().EndsWith(PCC_EXTENSION)) {
                 continue;
             }
-            string content = File.ReadAllText(file);
-            Debug.Log("Content: " + content);
-            LoadContent(content);
+            try {
+                ReadZip(file);
+            } catch (ImportException e) {
+                // TODO: Collect and forward errors to user.
+                Debug.LogWarning("Failed to import " + file + ": " + e.Message);
+            }
         }
     }
 
-    private void LoadContent(string properties) {
+    
+
+    
+
+    private void ReadZip(string file) {
+        using (ZipArchive zipArchive = ZipFile.Open(file, ZipArchiveMode.Read)) {
+            ZipArchiveEntry propertiesEntry = zipArchive.GetEntry("properties.json");
+            if (propertiesEntry == null) {
+                throw new ImportException("Content file did not contain a properties.json entry!");
+            }
+            using (StreamReader reader = new StreamReader(propertiesEntry.Open())) {
+                string properties = reader.ReadToEnd();
+                LoadContent(zipArchive, properties);
+            }
+        }
+    }
+
+    private void LoadContent(ZipArchive zipArchive, string properties) {
         BaseJsonModel baseModel = JsonUtility.FromJson<BaseJsonModel>(properties);
         switch (baseModel.type) {
             case "floor":
-                loadedFloors.Add(LoadFloor(properties));
+                loadedFloors.Add(LoadFloor(zipArchive, properties));
                 break;
             default:
                 throw new ImportException("Invalid content type: " + baseModel.type);
         }
     }
-
-    private Floor LoadFloor(string properties) {
+    
+    private Floor LoadFloor(ZipArchive zipArchive, string properties) {
         JsonFloor jsonFloor = JsonUtility.FromJson<JsonFloor>(properties);
-        // TODO: Import texture
+        
+        ZipArchiveEntry textureEntry = zipArchive.GetEntry("texture.png");
+        if (textureEntry == null) {
+            throw new ImportException("Floor file did not contain a texture.png entry.");
+        }
+        using (Stream stream = textureEntry.Open()) {
+            // TODO: Load texture
+        }
+        
         return new Floor(jsonFloor);
     }
 }
