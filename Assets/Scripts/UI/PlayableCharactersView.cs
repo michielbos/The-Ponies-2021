435c87378829f177689cdda5bc774da090078ca8
Michiel Bos <michielbos97@hotmail.com>
2019-06-15 02:48:05 +0200

    The number of character buttons is now updated with the number of ponies in the household.

---
+++ Assets/Scripts/UI/PlayableCharactersView.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Model.Ponies;
+using UnityEngine;
+
+namespace UI {
+
+public class PlayableCharactersView : MonoBehaviour {
+    public GameObject[] portraits;
+
+    private void OnEnable() {
+        List<Pony> ponies = PropertyController.Instance.property.household.ponies;
+        foreach (GameObject portrait in portraits) {
+            portrait.SetActive(false);
+        }
+        for (int i = 0; i < ponies.Count; i++) {
+            // TODO: Insert this pony into the portrait.
+            portraits[i].SetActive(true);
+        }
+    }
+}
+
+}
