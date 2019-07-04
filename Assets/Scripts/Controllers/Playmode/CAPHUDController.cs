91fb052800d9182eb2035e4e3ee70e1a4c455434
Michiel Bos <michielbos97@hotmail.com>
2019-07-04 02:12:35 +0200

    Applied autoformat, removed unused code.

--- Assets/Scripts/Controllers/Playmode/CAPHUDController.cs
+++ Assets/Scripts/Controllers/Playmode/CAPHUDController.cs
@@ -1,115 +1,64 @@
 ﻿using Assets.Scripts.Util;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-namespace Controllers.Playmode
-{
+namespace Controllers.Playmode {
 
-    public class CAPHUDController : SingletonMonoBehaviour<CAPHUDController>, IPointerEnterHandler, IPointerExitHandler
-    {
+public class CAPHUDController : SingletonMonoBehaviour<CAPHUDController> {
+    public Image cpanel;
 
-        public Image cpanel;
+    public Sprite noModeCpanel;
+    public Sprite bodyModeCpanel;
+    public Sprite headModeCpanel;
+    public Sprite eyesModeCpanel;
+    public Sprite maneModeCpanel;
+    public Sprite tailModeCpanel;
+    public Sprite outfitModeCpanel;
+    public Sprite accessoryModeCpanel;
+    public Sprite personalityModeCpanel;
 
-        public Sprite noModeCpanel;
-        public Sprite bodyModeCpanel;
-        public Sprite headModeCpanel;
-        public Sprite eyesModeCpanel;
-        public Sprite maneModeCpanel;
-        public Sprite tailModeCpanel;
-        public Sprite outfitModeCpanel;
-        public Sprite accessoryModeCpanel;
-        public Sprite personalityModeCpanel;
-
-        private bool touchingGui;
-
-     //   void Start()
-     //   {
-     //       MusicController.Instance.SwitchMusic(MusicType.NoMusic);
-     //   }
-
-        // Called from Unity GUI Button
-        public void ActivatePanel(int index)
-        {
-            ActivatePanel((CapHudPanel)index);
-        }
-
-        private static void ActivatePanel(CapHudPanel panel)
-        {
-            CAPModeController.GetInstance().SwitchMode(panel);
-        }
-
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            touchingGui = true;
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            touchingGui = false;
-        }
-
-        public bool IsMouseOverGui()
-        {
-            //I doubt how reliable this is, but it's not like we have anything better at the moment.
-            return touchingGui;
-        }
-
-        public void OnModeUpdate(CapHudPanel mode)
-        {
-            Debug.Log ("Swapping sprite, mode" + mode);
-            if (mode == CapHudPanel.Body)
-            {
-                cpanel.sprite = bodyModeCpanel;
-            }
-            else if (mode == CapHudPanel.Head)
-            {
-                cpanel.sprite = headModeCpanel;
-            }
-            else if (mode == CapHudPanel.Eyes)
-            {
-                cpanel.sprite = eyesModeCpanel;
-            }
-            else if (mode == CapHudPanel.Mane)
-            {
-                cpanel.sprite = maneModeCpanel;
-            }
-            else if (mode == CapHudPanel.Tail)
-            {
-                cpanel.sprite = tailModeCpanel;
-            }
-            else if (mode == CapHudPanel.Outfit)
-            {
-                cpanel.sprite = outfitModeCpanel;
-            }
-            else if (mode == CapHudPanel.Accessory)
-            {
-                cpanel.sprite = accessoryModeCpanel;
-            }
-            else if (mode == CapHudPanel.Personality)
-            {
-                cpanel.sprite = personalityModeCpanel;
-            }
-            else
-            {
-                cpanel.sprite = noModeCpanel;
-            }
-        }
+    // Called from Unity GUI Button
+    public void ActivatePanel(int index) {
+        ActivatePanel((CapHudPanel) index);
     }
 
-    public enum CapHudPanel
-    {
-        None = -1,
-        Body = 0,
-        Head = 1,
-        Eyes = 2,
-        Mane = 3,
-        Tail = 4,
-        Outfit = 5,
-        Accessory = 6,
-        Personality = 7,
+    private static void ActivatePanel(CapHudPanel panel) {
+        CAPModeController.GetInstance().SwitchMode(panel);
     }
 
+    public void OnModeUpdate(CapHudPanel mode) {
+        if (mode == CapHudPanel.Body) {
+            cpanel.sprite = bodyModeCpanel;
+        } else if (mode == CapHudPanel.Head) {
+            cpanel.sprite = headModeCpanel;
+        } else if (mode == CapHudPanel.Eyes) {
+            cpanel.sprite = eyesModeCpanel;
+        } else if (mode == CapHudPanel.Mane) {
+            cpanel.sprite = maneModeCpanel;
+        } else if (mode == CapHudPanel.Tail) {
+            cpanel.sprite = tailModeCpanel;
+        } else if (mode == CapHudPanel.Outfit) {
+            cpanel.sprite = outfitModeCpanel;
+        } else if (mode == CapHudPanel.Accessory) {
+            cpanel.sprite = accessoryModeCpanel;
+        } else if (mode == CapHudPanel.Personality) {
+            cpanel.sprite = personalityModeCpanel;
+        } else {
+            cpanel.sprite = noModeCpanel;
+        }
+    }
+}
 
+public enum CapHudPanel {
+    None = -1,
+    Body = 0,
+    Head = 1,
+    Eyes = 2,
+    Mane = 3,
+    Tail = 4,
+    Outfit = 5,
+    Accessory = 6,
+    Personality = 7,
+}
 
 }
--- Assets/Scripts/Controllers/Playmode/CAPModeController.cs
+++ Assets/Scripts/Controllers/Playmode/CAPModeController.cs
@@ -1,45 +1,36 @@
 ﻿using Assets.Scripts.Util;
 using UI;
 
-namespace Controllers.Playmode
-{
+namespace Controllers.Playmode {
 
-    public class CAPModeController : SingletonMonoBehaviour<CAPModeController>
-    {
-        public ModeButton bodyModeButton;
-        public ModeButton headModeButton;
-        public ModeButton eyesModeButton;
-        public ModeButton maneModeButton;
-        public ModeButton tailModeButton;
-        public ModeButton outfitModeButton;
-        public ModeButton accessoryModeButton;
-        public ModeButton personalityModeButton;
+public class CAPModeController : SingletonMonoBehaviour<CAPModeController> {
+    public ModeButton bodyModeButton;
+    public ModeButton headModeButton;
+    public ModeButton eyesModeButton;
+    public ModeButton maneModeButton;
+    public ModeButton tailModeButton;
+    public ModeButton outfitModeButton;
+    public ModeButton accessoryModeButton;
+    public ModeButton personalityModeButton;
 
+    public CapHudPanel CurrentMode { get; private set; } = CapHudPanel.None;
 
-        public CapHudPanel CurrentMode { get; private set; } = CapHudPanel.None;
+    public void SwitchMode(CapHudPanel mode) {
+        if (mode == CurrentMode)
+            mode = CapHudPanel.None;
+        CurrentMode = mode;
 
-        public void SwitchMode(CapHudPanel mode)
-        {
-            if (mode == CurrentMode)
-                mode = CapHudPanel.None;
-            CurrentMode = mode;
+        bodyModeButton.SetModeActive(mode == CapHudPanel.Body);
+        headModeButton.SetModeActive(mode == CapHudPanel.Head);
+        eyesModeButton.SetModeActive(mode == CapHudPanel.Eyes);
+        maneModeButton.SetModeActive(mode == CapHudPanel.Mane);
+        tailModeButton.SetModeActive(mode == CapHudPanel.Tail);
+        outfitModeButton.SetModeActive(mode == CapHudPanel.Outfit);
+        accessoryModeButton.SetModeActive(mode == CapHudPanel.Accessory);
+        personalityModeButton.SetModeActive(mode == CapHudPanel.Personality);
 
-
-            //CapCatalogController.GetInstance().CloseCatalog();
-
-
-            bodyModeButton.SetModeActive(mode == CapHudPanel.Body);
-            headModeButton.SetModeActive(mode == CapHudPanel.Head);
-            eyesModeButton.SetModeActive(mode == CapHudPanel.Eyes);
-            maneModeButton.SetModeActive(mode == CapHudPanel.Mane);
-            tailModeButton.SetModeActive(mode == CapHudPanel.Tail);
-            outfitModeButton.SetModeActive(mode == CapHudPanel.Outfit);
-            accessoryModeButton.SetModeActive(mode == CapHudPanel.Accessory);
-            personalityModeButton.SetModeActive(mode == CapHudPanel.Personality);
-
-            CAPHUDController.Instance.OnModeUpdate(mode);
-        }
-
+        CAPHUDController.Instance.OnModeUpdate(mode);
     }
+}
 
 }
