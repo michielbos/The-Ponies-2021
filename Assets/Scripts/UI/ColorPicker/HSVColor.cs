e4da67108c40fda16dd2c7680bd662dbe8f8f213
Oju <oju.programming@gmail.com>
2019-07-06 22:01:37 +0200

    Added the color converter system.

---
+++ Assets/Scripts/UI/ColorPicker/HSVColor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace UI.Utils.ColorPicker
+{
+    /// <summary>
+    /// Represents a color in the HUE model.
+    /// </summary>
+    [System.Serializable]
+    public struct HSVColor
+    {
+        #region HSV Model
+        /// <summary>
+        /// Hue value of the color.
+        /// </summary>
+        [SerializeField]
+        [Range(0f, 360f)]
+        public float hue;
+
+        /// <summary>
+        /// Saturation value of the color.
+        /// </summary>
+        [SerializeField]
+        [Range(0f, 1f)]
+        public float saturation;
+
+        /// <summary>
+        /// Value of the color, ie its brightness 
+        /// </summary>
+        [SerializeField]
+        [Range(0f, 1f)]
+        public float value;
+        #endregion
+    }
+}
