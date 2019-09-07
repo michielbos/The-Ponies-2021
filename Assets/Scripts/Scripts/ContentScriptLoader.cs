0a7487de447306951835f64d35e3532c2140cc5a
Michiel Bos <michielbos97@hotmail.com>
2019-09-07 17:04:14 +0200

    Fixed the mods path in ContentScriptLoader.

--- Assets/Scripts/Scripts/ContentScriptLoader.cs
+++ Assets/Scripts/Scripts/ContentScriptLoader.cs
@@ -20,7 +20,7 @@ public class ContentScriptLoader : ScriptLoaderBase {
     private string GetPath(string name) {
         // Note that we currently don't prevent scripts from loading files outside the mod/content directories.
         if (name.StartsWith("mods/")) {
-            return Application.dataPath + "../Mods/Scripts/" + name.Substring(5);
+            return Application.dataPath + "/../Mods/Scripts/" + name.Substring(5);
         }
         return Application.dataPath + "/Content/Scripts/" + name;
     }
