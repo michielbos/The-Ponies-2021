9af54404849208caa826a9bf5a1043706df8368a
Michiel Bos <michielbos97@hotmail.com>
2020-01-07 21:16:44 +0100

    Actions are now "forcefully canceled" if they throw an unhandled exception or lose their target. Added OnTargetLost and OnForceCancel methods which can be overridden to do action-specific cleanups. (PonyAction was missing?)

--- Assets/Scripts/Model/Actions/PonyAction.cs
+++ Assets/Scripts/Model/Actions/PonyAction.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Model.Data;
 using Model.Ponies;
+using UnityEngine;
 
 namespace Model.Actions {
 
+/// <summary>
+/// Base class for all queue actions that a pony can do.
+/// </summary>
 public abstract class PonyAction {
     public readonly string identifier;
     public readonly Pony pony;
@@ -41,10 +46,24 @@ public abstract class PonyAction {
         if (tickCount == 1) {
             OnStart();
         }
-        if (Tick()) {
-            Finish();
+        if (!TargetExists()) {
+            finished = true;
+            OnTargetLost();
+            return;
+        }
+        try {
+            if (Tick()) {
+                Finish();
+            }
+        } catch (Exception e) {
+            Debug.LogError("Unhandled error on action: " + identifier);
+            Debug.LogException(e);
+            finished = true;
+            OnForceCancel();
         }
     }
+
+    protected internal abstract bool TargetExists();
     
     /// <summary>
     /// Called every ingame second to execute this action's behaviour.
@@ -86,8 +105,30 @@ public abstract class PonyAction {
         }
     }
     
+    /// <summary>
+    /// Called when a pony has completed its action, regardless of whether it was successful, failed or canceled.
+    /// If OnForceCancel hasn't been implemented, this is also called when an action is forcefully canceled.
+    /// </summary>
     protected virtual void OnFinish() {
         // Override for additional finish behaviour.
+    }
+
+    /// <summary>
+    /// Called when the target of the pony went missing (for example, was sold by the player).
+    /// No more ticks will be called after this action.
+    /// If not implemented, this method will call OnForceCancel instead. (which calls OnFinish when not implemented)
+    /// </summary>
+    protected virtual void OnTargetLost() {
+        OnForceCancel();
+    }
+    
+    /// <summary>
+    /// Called when the action was forcefully canceled (for example, because of an unhandle exception or a missing target).
+    /// No more ticks will be called after this action.
+    /// If not implemented, this method will call OnFinish instead.
+    /// </summary>
+    protected virtual void OnForceCancel() {
+        OnFinish();
     }
 
     public abstract PonyActionData GetData();
