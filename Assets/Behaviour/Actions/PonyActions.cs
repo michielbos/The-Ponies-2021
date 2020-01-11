ebcfb4177811cc03c883e36ba7c687ae4d35b14f
Michiel Bos <michielbos97@hotmail.com>
2020-01-11 23:19:20 +0100

    Ponies can now bathe in bathtubs and showertubs.

--- Assets/Behaviour/Actions/PonyActions.cs
+++ Assets/Behaviour/Actions/PonyActions.cs
@@ -6,7 +6,8 @@ public static class PonyActions {
     public static void RegisterActionProviders() {
         ActionManager.AddObjectActionProviders(new IObjectActionProvider[] {
             new SeatActionProvider(),
-            new ToiletActionProvider()
+            new ToiletActionProvider(),
+            new ShowerBathActionProvider(),
         });
         ActionManager.AddTileActionProviders(new [] {
             new MoveActionProvider()
