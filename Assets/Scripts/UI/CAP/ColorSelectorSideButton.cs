1c55bdeacf4c732c95086e21bdc69277ad21f904
Quaint Hope <hopequaint@gmail.com>
2020-06-28 19:19:45 +0200

    Restarting on a cleaner basis after a while.

---
+++ Assets/Scripts/UI/CAP/ColorSelectorSideButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.CAP
+{
+    /// <summary>
+    /// The button behaviour for the buttons on the Color Selector Side Panel in the Create a Pony UI.
+    /// </summary>
+    internal class ColorSelectorSideButton : MonoBehaviour
+    {
+        /// <summary>
+        /// The title to display on the Color Selector Panel.
+        /// </summary>
+        [SerializeField]
+        internal string associatedTitle = "Color";
+
+        /// <summary>
+        /// The image used to display the selected color.
+        /// </summary>
+        [SerializeField]
+        private Image _colorDisplayer = null;
+    }
+}
